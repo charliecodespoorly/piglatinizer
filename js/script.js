270c685e0a0e9f4641b303e81d9707f7f76da60b
charliecodespoorly <charliemckennakvoriak@gmail.com>
2019-10-03 01:35:00 +0000

    no time for a message

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 $( "document" ).ready(function() {
+    $("#button").click(function(){
+            var input = $(".input").val();
+            $(".output").html("<p>"+input+"</p>")
+    });
 
+    function wordconvert(word){
+        return(word.slice(1,word.length)+word.slice(0,1)+"ay");
+    }
 
-
+    function phraseconvert(phrase){
+        words = phrase.split(" ");
+        for(var i = 0; i < words.length);
+    }
 });
