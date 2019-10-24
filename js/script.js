702104ca5189e9e24765c9ee688f847c122bf815
Charles Kvoriak <charliemckennakvoriak@gmail.com>
2019-10-24 01:24:31 +0000

    Javascipt now can generate the end of a word(first sount plus ay)

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 $( "document" ).ready(function() {
     $("#button").click(function(){
         var input = $(".input").val();
-        alert(input);
-        $(".output").html("<p>"+phraseconvert(input)+"</p>")
+        // alert(phraseconvert(input));
+        $(".output").text(phraseconvert(input))
     });
 
     function wordconvert(word){
-        return(word.slice(1,word.length)+word.slice(0,1)+"ay");
+        // alert("eagle".match("/$[aeiou]+/g"));
+        if(word.match(/^[aeiou]+/)){
+          return(word+"ay")
+        }
+
+        else{
+          return(word.match(/^[^aeiou]+/)+"ay ");
+        }
     }
 
     function phraseconvert(phrase){
         words = phrase.split(" ");
         var output = "";
-        for(var i = 0; i < words.length, i++;){
+        for(var i = 0; i < words.length; i++){
             output = output + wordconvert(words[i]);
         }
-        return output;
+        return output.toLowerCase();
     }
 });
